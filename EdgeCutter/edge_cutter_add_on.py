5a690cf6478d9a666fb486cc8343824c6a2c0c4d
dschwant <dschwant@gmail.com>
2017-07-18 12:30:31 -0700

    selet object and button work

--- EdgeCutter/edge_cutter_add_on.py
+++ EdgeCutter/edge_cutter_add_on.py
@@ -1,8 +1,3 @@
-import bpy
-import math
-from mathutils import Vector
-from functools import reduce
-
 bl_info = {
     "name": "Edge_Rotate_Cutter",
     "author": "DS",
@@ -12,8 +7,14 @@ bl_info = {
     "warning": "",
     "category": "Object"}
 
+import bpy
+import math
+from mathutils import Vector
+from functools import reduce
+
 class ToolsPanel(bpy.types.Panel):
     bl_label = "Edge Rotate Cutter"
+    bl_idname = "VIEW_3D_TOOLS_Edge_Rotate_Cutter"
     bl_space_type = "VIEW_3D"
     bl_region_type = "TOOLS"
     bl_category = "Cutter"
@@ -23,8 +24,9 @@ class ToolsPanel(bpy.types.Panel):
         layout = self.layout
         scene = context.scene
 
-        #layout.prop_search(scene, "theCutter", scene, "objects")
-        layout.operator("cutter.cut", text="Do Cuts")
+        col = layout.column(align=True)
+        col.prop_search(scene, "Cutter", scene, "objects")
+        col.operator("cutter.cut", text="Do Cuts")
 
 class OBJECT_CutButton(bpy.types.Operator):
     bl_idname = "cutter.cut"
@@ -33,16 +35,22 @@ class OBJECT_CutButton(bpy.types.Operator):
  
     def execute(self, context):
         if self.country == '':
+            print(context.scene.Cutter)
             print("Make Cuts")
         else:
             print("Don't Make Cuts from %s!" % self.country)
-        return{'FINISHED'}   
-    
+        return {"FINISHED"}
+
 def register():
     bpy.utils.register_class(ToolsPanel)
- 
+    bpy.utils.register_class(OBJECT_CutButton)
+    bpy.types.Scene.Cutter = bpy.props.StringProperty()
+
+
 def unregister():
     bpy.utils.unregister_class(ToolsPanel)
- 
-if __name__ == "__main__":  # only for live edit.
-    bpy.utils.register_module(__name__)
+    bpy.utils.unregister_class(OBJECT_CutButton)
+    del bpy.types.Object.Cutter
+
+if __name__ == "__main__":
+    register()
